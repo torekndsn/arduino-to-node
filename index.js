328e9fef976d7292f7f7adfc9c88180593ec75c7
torekndsn <storetoreknudsen@gmail.com>
2017-09-25 21:34:33 +0200

    working locally

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-
-var express = require("express");
-var app = express(); 
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname+ '/public'));
+app.use(express.static(__dirname + '/public'));
 
 var potentiometer = 0; 
 
-app.get('/set_value', function(request, response){
-	potentiometer = request.query.val;
-	respone.send('potentiometer value set to ' + potentiometer);
+app.get('/set_pressure', function(request, response) {
+  potentiometer = request.query.val;
+  response.send('potentiometer set to '+potentiometer);
 });
 
-app.get('/set_value', function(request, response){
-	respone.send(String(potentiometer));
+app.get('/get_pressure', function(request, response) {
+  response.send(String(potentiometer));
 });
 
-app.listen(app.get('port'), function(){
-	console.log("Node app is running at localHost:" + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
 
